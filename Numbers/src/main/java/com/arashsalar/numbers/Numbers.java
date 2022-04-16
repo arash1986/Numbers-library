2af279f863dfeabcaf9595c145b61051f2d5d250
a.abdorrahimi <arash.abd1986@gmail.com>
2022-04-16 09:15:33 +0430

    first commit

---
+++ Numbers/src/main/java/com/arashsalar/numbers/Numbers.java
@@ -0,0 +1,72 @@
+package com.arashsalar.numbers;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class Numbers {
+    private static final String[] persianNumbers = new String[]{"۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"};
+
+    public static String persianNumber(String text) {
+        if (text.length() == 0) {
+            return "";
+        }
+        String out = "";
+        int length = text.length();
+        for (int i = 0; i < length; i++) {
+            char c = text.charAt(i);
+            if ('0' <= c && c <= '9') {
+                int number = Integer.parseInt(String.valueOf(c));
+                out += persianNumbers[number];
+            } else if (c == '٫') {
+                out += '،';
+            } else {
+                out += c;
+            }
+        }
+        return out;
+    }
+
+    //convert English to Persian
+    public static String EntoFa(String num) {
+        return num
+                .replace("0", "۰")
+                .replace("1", "۱")
+                .replace("2", "۲")
+                .replace("3", "۳")
+                .replace("4", "۴")
+                .replace("5", "۵")
+                .replace("6", "۶")
+                .replace("7", "۷")
+                .replace("8", "۸")
+                .replace("9", "۹");
+    }
+
+    //convert Persian to English
+    public static String FatoEn(String num) {
+        return num
+                .replace("۰", "0")
+                .replace("۱", "1")
+                .replace("۲", "2")
+                .replace("۳", "3")
+                .replace("۴", "4")
+                .replace("۵", "5")
+                .replace("۶", "6")
+                .replace("۷", "7")
+                .replace("۸", "8")
+                .replace("۹", "9");
+    }
+
+
+    /* for long values that need seprator */
+    public static String currency(String num) {
+        try {
+            long number = Integer.parseInt(num);
+            NumberFormat formatter = new DecimalFormat("#,###,###,###");
+            return persianNumber(formatter.format(number));
+        }catch (Exception e){
+
+            return num;
+        }
+    }
+}
+
